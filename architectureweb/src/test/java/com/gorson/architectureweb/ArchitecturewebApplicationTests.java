d84ebdca1624a79cbe5cd64c716b14e94503a5f5
GorsonLee <919501050@qq.com>
2017-08-26 12:11:40 +0800

    [add]添加Druid配置文件

--- architectureweb/src/test/java/com/gorson/architectureweb/ArchitecturewebApplicationTests.java
+++ architectureweb/src/test/java/com/gorson/architectureweb/ArchitecturewebApplicationTests.java
@@ -1,7 +1,9 @@
 package com.gorson.architectureweb;
 
+import com.gorson.architectureweb.config.DruidConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ArchitecturewebApplicationTests {
 
+	@Autowired
+	private DruidConfig druidConfig;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(druidConfig.toString());
 	}
 
 }
